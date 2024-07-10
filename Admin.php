c8258d1aaadc119c66ab7c8c97a7367291d53e63
hamza ait bouhou <159446531+s4nji1@users.noreply.github.com>
2024-07-10 12:23:34 +0100

    DataTable

--- Admin.php
+++ Admin.php
@@ -11,6 +11,7 @@ include 'DataBase.php';
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" type="image/png" href="Images/logo.png">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
 </head>
 <body>
     <div class="wrapper">
@@ -30,7 +31,7 @@ include 'DataBase.php';
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-3 sidebar bg-light py-3" style="width: 50%;">
-                    <table class="table table-hover">
+                    <table id="uploadsTable" class="table table-hover">
                         <thead class="thead-dark">
                             <tr>
                                 <th>Name</th>
@@ -82,13 +83,18 @@ include 'DataBase.php';
         </footer>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script>
+        $(document).ready(function() {
+            $('#uploadsTable').DataTable();
+        });
+
         function viewDocument(filePath) {
             document.getElementById('document-viewer').src = filePath;
         }
     </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
